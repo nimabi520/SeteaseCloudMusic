25e7b205c1718b40fc1ba6def8d59212aa7703f7
halo <1064307133@qq.com>
2026-03-12 20:43:02 +0800

    确定初始开发方向

--- app/src/main/java/com/example/seteasecloudmusic/present/MainActivity.kt
+++ app/src/main/java/com/example/seteasecloudmusic/present/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.seteasecloudmusic
+package com.example.seteasecloudmusic.present
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.seteasecloudmusic.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
