5c2274e881041dde3284c743353ef34a8a257a1d
halo <1064307133@qq.com>
2026-03-14 23:40:19 +0800

    重构网络模块，增加 OkHttpClient 配置和基础 URL 提供方法

--- app/src/main/java/com/example/seteasecloudmusic/di/NetworkModule.kt
+++ app/src/main/java/com/example/seteasecloudmusic/di/NetworkModule.kt
@@ -1,22 +1,36 @@
 package com.example.seteasecloudmusic.di
 
 import com.example.seteasecloudmusic.data.api.NeteaseMusicService
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class NetworkModule {
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl("http://57.158.26.135:3000/")
-            .addConverterFactory(GsonConverterFactory.create())  // 设置解析数据时，使用的转换库
+
+    private fun provideBaseUrl(): String = "http://57.158.26.135:3000/";
+
+    private fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS) // 设置连接超时时间
+            .readTimeout(30, TimeUnit.SECONDS)    // 设置读取超时时间
+            .writeTimeout(30, TimeUnit.SECONDS)   // 设置写入超时时间
+            //TODO:以后记得回来添加拦截器
             .build()
     }
 
-    private val musicService: NeteaseMusicService by lazy {
-        retrofit.create(NeteaseMusicService::class.java)
+    private fun provideRetrofit(client: OkHttpClient): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(provideBaseUrl())
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create()) // 添加 Gson 转换器
+            .build()
     }
 
     fun provideNeteaseMusicService(): NeteaseMusicService {
-        return musicService
+        val client: OkHttpClient = provideHttpClient()
+        val retrofit: Retrofit = provideRetrofit(client)
+        return retrofit.create(NeteaseMusicService::class.java)
     }
 }
