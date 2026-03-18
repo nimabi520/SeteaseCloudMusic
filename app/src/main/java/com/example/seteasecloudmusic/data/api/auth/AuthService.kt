09cb99c4dad40ac0c0844b9997c899018b7d7d8b
吴与同 <1064307133@qq.com>
2026-03-18 11:36:15 +0800

    完成了二维码登录对应类

--- app/src/main/java/com/example/seteasecloudmusic/data/api/auth/AuthService.kt
+++ app/src/main/java/com/example/seteasecloudmusic/data/api/auth/AuthService.kt
@@ -2,10 +2,10 @@ package com.example.seteasecloudmusic.data.api.auth
 
 import retrofit2.http.POST
 import retrofit2.http.Query
-import com.example.seteasecloudmusic.data.model.auth.LoginResponseDto
-import com.example.seteasecloudmusic.data.model.auth.QrKeyResponseDto
-import com.example.seteasecloudmusic.data.model.auth.QrCodeResponseDto
-import com.example.seteasecloudmusic.data.model.auth.QrStatusResponseDto
+import com.example.seteasecloudmusic.data.model.auth.LoginResponseDao
+import com.example.seteasecloudmusic.data.model.auth.QrKeyResponseDao
+import com.example.seteasecloudmusic.data.model.auth.QrCodeResponseDao
+import com.example.seteasecloudmusic.data.model.auth.QrStatusResponseDao
 import java.sql.Timestamp
 
 /**
@@ -16,25 +16,25 @@ interface AuthService {
     suspend fun loginWithPassword(
         @Query("phone") phone: String,
         @Query("password") password: String
-    ): LoginResponseDto
+    ): LoginResponseDao
 
     @POST("/login/qr/key")
     suspend fun getQrKey (
         @Query("timestamp")timestamp: Timestamp
-    ): QrKeyResponseDto
+    ): QrKeyResponseDao
 
     @POST("/login/qr/create")
     suspend fun getQrCode(
         @Query("key")key: String,
         @Query("timestamp")timestamp: Timestamp,
         @Query("qrimg")qrimg: Int
-    ): QrCodeResponseDto
+    ): QrCodeResponseDao
 
     @POST("/login/qr/check")
     suspend fun checkQrCodeStatus(
         @Query("key")key: String,
         @Query("timestamp")timestamp: Timestamp
-    ): QrStatusResponseDto
+    ): QrStatusResponseDao
 
 
 }
--- app/src/main/java/com/example/seteasecloudmusic/data/model/auth/LoginResponseDto.kt
+++
@@ -1,7 +0,0 @@
-package com.example.seteasecloudmusic.data.model.auth
-
-/**
- * 登录接口返回体占位模型，后续按接口字段补充。
- */
-class LoginResponseDto {
-}
