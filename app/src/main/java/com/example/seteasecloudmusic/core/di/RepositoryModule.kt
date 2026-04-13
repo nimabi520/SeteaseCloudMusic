3d6303c19f5d4ea784faf4963bfefed0d47a9bbc
Fantto00 <1531849260@qq.com>
2026-04-13 13:24:19 +0800

    pref:引入hilt依赖注入框架，修复ViewModel配置变更后的状态丢失问题

---
+++ app/src/main/java/com/example/seteasecloudmusic/core/di/RepositoryModule.kt
@@ -0,0 +1,18 @@
+package com.example.seteasecloudmusic.core.di
+
+import com.example.seteasecloudmusic.feature.search.data.SearchRepositoryImpl
+import com.example.seteasecloudmusic.feature.search.domain.SearchRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindSearchRepository(
+        impl: SearchRepositoryImpl
+    ): SearchRepository
+}
